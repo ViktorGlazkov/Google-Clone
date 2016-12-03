f65d9979f77158c20a2d0f9a6260b7ca3a676294
Viktor Glazkov <viktorg.dev@gmail.com>
2016-12-03 12:45:21 +0200

    Add controller for indexing

--- src/main/java/core/SearchController.java
+++ src/main/java/core/SearchController.java
@@ -1,13 +1,22 @@
 package core;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class SearchControllerController {
+public class SearchController {
     @RequestMapping(value = {"/index"}, method = RequestMethod.GET)
     public String getIndexControllers() {
+        return "index_page";
+    }
+
+    @RequestMapping(value = {"/index"}, method = RequestMethod.POST)
+    public String indexPage(Model model, @RequestParam String q) {
+        System.out.println(q);
+        model.addAttribute("q", q);
         return "index_page";
     }
 
