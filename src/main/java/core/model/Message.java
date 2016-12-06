06b22dac8cdad28a1fd2a5d9f1931797f620c661
Viktor Glazkov <viktorg.dev@gmail.com>
2016-12-06 19:06:17 +0200

    Add Model

---
+++ src/main/java/core/model/Message.java
@@ -0,0 +1,30 @@
+package core.model;
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.ScoreDoc;
+
+import java.io.IOException;
+
+public class Message {
+    private String title;
+    private String url;
+
+
+    public Message(ScoreDoc hit, IndexReader reader) {
+        try {
+            this.title = reader.document(hit.doc).get("title");
+            this.url = reader.document(hit.doc).get("link");
+        } catch (IOException e) {
+            System.err.println("Can't read document:" + hit.doc);
+            e.printStackTrace();
+        }
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
