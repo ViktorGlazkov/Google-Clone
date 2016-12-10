ec6dea8dad0c5cbd410c87135193ce41e2e81d59
Viktor Glazkov <viktorg.dev@gmail.com>
2016-12-10 15:35:36 +0200

    Replace logic from crawlerService to DocFactory. Improve algorithm of htmlParsing. Now it parses page only once time.

--- src/main/java/core/indexer/DocumentFactory.java
+++ src/main/java/core/indexer/DocumentFactory.java
@@ -1,13 +1,69 @@
 package core.indexer;
 
+import core.model.Crawler;
+import core.html.HtmlService;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.FieldType;
 import org.apache.lucene.index.IndexOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class DocumentFactory {
+    @Autowired
+    private HtmlService htmlService;
 
-    public static Document createWith(String titleStr, String bodyStr, String url) {
+    private Crawler crawler;
+    private List<Document> documents;
+
+    public List<Document> getDocuments() {
+        this.crawler = Crawler.getInstance();
+        this.documents = new ArrayList<>();
+
+        String htmlPage = htmlService.getHTML(crawler.getSeed());
+        parseDocuments(htmlPage, 0);
+        return documents;
+    }
+
+    private void parseDocuments(String html, int step) {
+        if (step == crawler.getDepth()) {
+            return;
+        }
+
+        String seed = crawler.getSeed();
+
+        for (String url : htmlService.getLinks(html, seed)) {
+            if (!crawler.getLinks().contains(url)) {
+                crawler.getLinks().add(url);
+
+                String htmPage = htmlService.getHTML(url);
+                this.documents.add(getDocument(htmPage, url));
+                parseDocuments(htmPage, step + 1);
+            }
+        }
+    }
+
+    private Document getDocument(String html, String url) {
+
+        String title = htmlService.getTitle(html);
+        String content = htmlService.getContent(html);
+
+        if (title == null) {
+            title = "";
+        }
+
+        if (content == null) {
+            content = "";
+        }
+
+        return createWith(title, content, url);
+    }
+
+    private static Document createWith(String titleStr, String bodyStr, String url) {
         Document document = new Document();
 
         FieldType textIndexedType = new FieldType();
