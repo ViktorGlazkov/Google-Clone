2d0f4050802eed675e7a4e42dea0a2cdb240a434
Viktor Glazkov <viktorg.dev@gmail.com>
2016-12-19 12:39:26 +0200

    Add beans for IndexSearcher and  MultiFieldQueryParser

--- src/main/java/config/WebMvcConfig.java
+++ src/main/java/config/WebMvcConfig.java
@@ -1,5 +1,10 @@
 package config;
 
+import core.indexer.IndexerService;
+import org.apache.lucene.analysis.ru.RussianAnalyzer;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +14,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import java.io.IOException;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "core")
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Autowired
+    IndexerService indexerService;
 
     @Bean
     public InternalResourceViewResolver internalResourceViewResolver() {
@@ -26,5 +35,17 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public IndexSearcher indexSearcher() throws IOException {
+        return new IndexSearcher(indexerService.readIndex());
+    }
+
+    @Bean
+    public MultiFieldQueryParser queryParser() {
+        return new MultiFieldQueryParser(
+                new String[]{"body", "title"},
+                new RussianAnalyzer());
     }
 }
